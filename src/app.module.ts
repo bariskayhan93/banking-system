be5f9a0706a3451a4b14715e5e02fd4e5f142783
Baris Kayhan <bariskayhan53@gmail.com>
2025-06-14 16:02:41 +0200

    Add feature modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { PersonModule } from './modules/person/person.module';
+import { BankAccountModule } from './modules/bank-account/bank-account.module';
+import { TransactionModule } from './modules/transaction/transaction.module';
+import { TestModule } from './modules/test/test.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PersonModule, BankAccountModule, TransactionModule, TestModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
